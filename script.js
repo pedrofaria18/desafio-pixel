a395f7c4fa389084ae392a0489322c2408255b2a
pedrofaria18 <64284346+pedrofaria18@users.noreply.github.com>
2021-04-17 21:23:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const context = canvas.getContext("2d");
 // Desenha img no canvas, com a imagem e as cordenadas desejadas
 context.drawImage(img, 0, 0);
 
-//Desenhando o canvas pixel a pixel
+//Ler o canvas pixel a pixel
 const data = context.getImageData(0, 0, canvas.width, canvas.height).data;
 
 // Para cada pixel temos que definir as cores red(r), green(g), blue(b)
@@ -19,7 +19,7 @@ var r, g, b;
 // Vetor para armezenar as cores
 const coresPresentes = {};
 
-for (let i = 0; n = data.length, i < n; i += 4) {
+for (let i = 0; i < data.length; i += 4) {
   r = data[i]; // red
   g = data[i + 1]; // green
   b = data[i + 2]; // blue
@@ -34,6 +34,11 @@ for (let i = 0; n = data.length, i < n; i += 4) {
   }
 }
 
+// Transformando a imagem de RGB para HEX
+function rgbToHex(r, g, b) {
+  return "#" + numberToHex(r) + numberToHex(g) + numberToHex(b);
+}
+
 function numberToHex(number) {
   var hex = number.toString(16);
   if (hex.length < 2) {
@@ -42,9 +47,4 @@ function numberToHex(number) {
   return hex;
 }
 
-// Transformando a imagem de RGB para HEX
-function rgbToHex(r, g, b) {
-  return "#" + numberToHex(r) + numberToHex(g) + numberToHex(b);
-}
-
-console.log(JSON.stringify(coresPresentes));
+console.log(coresPresentes);
