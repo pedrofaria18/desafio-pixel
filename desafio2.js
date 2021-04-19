12dcc83c0f9207f9abc04f7a9338c5bc24e9eca0
pedrofaria18 <64284346+pedrofaria18@users.noreply.github.com>
2021-04-19 11:09:34 -0300

    Inserindo tentativa de transformar as cores em binário

--- desafio2.js
+++ desafio2.js
@@ -11,3 +11,26 @@ context.drawImage(img, 0, 0, canvas.width, canvas.height);
 */
 var base64 = canvas.toDataURL();
 console.log(base64);
+
+
+
+/*
+  Para 3 cores achadas, tentar montar um número binário válido
+*/
+const data = context.getImageData(0, 0, canvas.width, canvas.height).data;
+const p = document.querySelector('p');
+var r, g, b;
+
+for (let i = 0; i < data.length; i+=4) {
+  r = data[i];
+  g = data[i + 1];
+  b = data[i + 2];
+
+  const rgb = `rgb(${r}, ${g}, ${b})`;
+
+  if(rgb == "rgb(255, 255, 255)") {
+    p.innerHTML += 0
+  } else if(rgb == "rgb(96, 192, 0)") {
+    p.innerHTML += 1
+  }
+}
