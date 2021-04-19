cdcc87663d7b9b21a211e456e979b8daa765404e
pedrofaria18 <64284346+pedrofaria18@users.noreply.github.com>
2021-04-19 19:02:10 -0300

    Update desafio2.js

--- desafio2.js
+++ desafio2.js
@@ -1,17 +1,17 @@
+// Modelo padrão de criação do canvas
 const img = document.querySelector('img');
-
 const canvas = document.createElement("canvas");
 canvas.width = img.width;
 canvas.height = img.height;
 const context = canvas.getContext("2d");
 context.drawImage(img, 0, 0, canvas.width, canvas.height);
 
+
 /*
   Img to Base64
 */
 var base64 = canvas.toDataURL();
 console.log(base64);
-
 
 
 /*
